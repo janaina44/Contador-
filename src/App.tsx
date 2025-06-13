d59e6746d7ebef1a5b8aa8d30e22c26a1d56c17c
Janaina Da Silva <janinha4488@gmail.com>
2025-06-13 14:02:00 -0300

    Primeiro commit: projeto contador com React e TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';// importamos o React para usar JSX(a linguagem que parece HTML dentro do JavaScript).
+
 import './App.css';
 
+// Criamos uma função chamada App, que retorna (ou seja, mostra) o que queremos ver na tela.
 function App() {
+  // Criamos uma "caixinha de memória " chamada contador e começamos com 0
+  const [contador, setContador] = useState<number>(0);
+  useEffect(() => { // Diz: "Ei React, quero rodar um código assim que a pagina abrir."
+    const numeroSalvo = localStorage.getItem('contador'); // vai até o localStorage do navegador e procura se tem algo guardado com o nome contador 
+    if (numeroSalvo !== null) {
+      setContador(Number(numeroSalvo))
+    }
+  }, []); // Esses [] dizem ao React "rode esse código só uma vez, quando a página abrir."
+
+  useEffect(() => {
+    localStorage.setItem('contador', String(contador));
+  }, [contador]); // Esse colchetes dizem: "Rode esse efeito sempre que o valor do contador mudar."
+
+  // função para aumentar
+  const aumentar = () => {
+    setContador(contador + 1);
+  };
+
+  // função para diminuir
+  const diminuir = () => {
+    setContador(contador - 1);
+  }
+
+  const resetar = () => {
+    setContador(0);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <main className="container">
+    <h1>Contador</h1>
+    <p className="numero">{contador}</p>
+    <div className="botoes">
+      <button onClick={aumentar}>+</button>
+      <button onClick={diminuir}>-</button>
+      <button onClick={resetar}>Resetar</button>
     </div>
-  );
+  </main>
+);
 }
 
-export default App;
+export default App; // Essa função App pode ser usada em outros arquivos.
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // importa o react, que é a biblioteca que usamos para criar os componentes da interface(como botões, textos, etc.)
+import ReactDOM from 'react-dom/client';// importa o ReactDom, que é o conector entre o React e o navegador.Ele sabe como mostrar o React na tela.
+import App from './App';// Estamos dizendo "Quero usar o componente App" que é o coração do nosso site. Tudo que vemos na tela vem de APP.tsx.
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);/* Aqui o React está procurando um lugar dentro do HTML para mostrar o site. No arquivo public/index.html, existe uma div assim: <div id "root></div> Esse é o lugar onde o React vai desenhar seu site."*/
+
+
+// Essa parte mostra o que deve aparecer na tela
+root.render( 
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/> 
+  </React.StrictMode>);// ajuda a encontrar erros e boas praticas durante o desenvolvimento
+  // <App/> mostra o conteudo do componente app. 
